bb7985f5714362797676db347375df29d58f981c
Dmytro Ilnytskyy <dimitri.ilnitski@gmail.com>
2023-03-14 21:28:36 +0200

    14.03.2023

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Logo from './components/Logo/Logo';
+import Name from './components/Logo/Name';
 import Button from '../../common/Button/Button';
 
 function Header() {
 	return (
 		<>
 			<Logo />
+			<Name />
 			<Button buttonText={'Logout'} />
 		</>
 	);
